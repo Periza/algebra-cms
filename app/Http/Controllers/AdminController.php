4975e0ece0a66aec65c47b2e1a4fc049d0a74a6e
Marko Perica <marko.perica2@gmail.com>
2022-07-24 02:51:07 +0200

    added AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('admin');
+    }
+
+    public function getAdministrationView() {
+        $nonAdminUsers = User::where('role_id', '=', 'null')->orderBy('created_at')->get();
+        $nonAdminUsers = User::get();
+        return view('administration', ['users' => $nonAdminUsers, 'roles' => Role::get()]);
+    }
+
+    public function getRolesView() {
+        return view('roles', ['roles' => Role::get()]);
+    }
+
+    public function newRole() {
+        return view('roleNew');
+    }
+
+    public function roleInsert(Request $request) {
+        $role = new Role;
+        $role->name = $request->name;
+        $role->save();
+        return redirect('/roles');
+    }
+
+    public function roleDelete(Request $request, Role $role) {
+        $role->delete();
+        return redirect('/roles');
+    }
 }
--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 
 class RegisterController extends Controller
 {
